63917d0b206d7daa61a1d63a9c1fd266b24d7370
athomas227 <andrewthomas@pursuit.org>
2023-12-11 20:07:18 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EffectTimer from './EffectTimer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EffectTimer />
+    </div>
+  );
+}
+
+export default App;
