e3d4339a2f5cc888438e0172518e5a693c193326
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-01-14 07:50:15 -0500

    added EffectTimer component

---
+++ my-app-3.17/src/EffectTimer.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+function EffectTimer(){
+    const [seconds, setSeconds] = useState(0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(prevSeconds => prevSeconds + 1);
+        }, 1000);
+        
+        return () => clearInterval(interval);
+    }, []);
+    return <h1>{seconds} seconds have passed</h1>;
+}
+
+export default EffectTimer;
